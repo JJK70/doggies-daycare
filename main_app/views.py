19f26193468eaa6f35fde4081675e2e5fe31c03b
JJK70 <jjk701@gmail.com>
2022-11-04 15:57:28 -0400

    setup migration and admin

--- main_app/views.py
+++ main_app/views.py
@@ -1,28 +1,27 @@
 from django.shortcuts import render
+from .models import Dog
 
+# class Dog:
+#   def __init__(self, name, breed, description, age):
+#     self.name = name
+#     self.breed = breed
+#     self.description = description
+#     self.age = age
 
-from django.http import HttpResponse
-
-class Dog:
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
-
-dogs = [
-  Dog('Tyrion', 'teddy bear', 'Needy', 6),
-  Dog('Fern', 'mutt', 'Hyper', 3),
-  Dog('Vanna', 'black lab', 'Former guide dog', 4),
-  Dog('Winston', 'american bulldog', 'Has puppy breath', 0)
-]
+# dogs = [
+#   Dog('Tyrion', 'teddy bear', 'Needy', 6),
+#   Dog('Fern', 'mutt', 'Hyper', 3),
+#   Dog('Vanna', 'black lab', 'Former guide dog', 4),
+#   Dog('Winston', 'american bulldog', 'Has puppy breath', 0)
+# ]
 
 
 def home(request):
-  return HttpResponse('<h1>Hello Doggies!</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
 
 def dogs_index(request):
+  dogs = Dog.objects.all()
   return render(request, 'dogs/index.html', { 'dogs': dogs })
