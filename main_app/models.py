ea77018cfb41eafbfba2e452a7ea317e469cfd1f
JJK70 <jjk701@gmail.com>
2022-11-07 17:44:35 -0500

    add form and ability to add feeding

--- main_app/models.py
+++ main_app/models.py
@@ -31,4 +31,7 @@ class Feeding(models.Model):
   dog = models.ForeignKey(Dog, on_delete=models.CASCADE)
   
   def __str__(self):
-    return f"{self.get_meal_display()} on {self.date}" 
+    return f"{self.get_meal_display()} on {self.date}"
+  
+  class Meta:
+    ordering =['-date']
